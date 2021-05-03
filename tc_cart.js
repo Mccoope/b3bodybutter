5baecc99b09cf5badd5171713a782b45c496c6d0
Mccoope <83585263+Mccoope@users.noreply.github.com>
2021-05-03 16:42:25 -0400

    Add files via upload

---
+++ tc_cart.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: Myckieala Cooper
+   Date:   4/18/21
+   
+   Filename: tc_cart.js
+	
+*/
+var orderTotal = new Array(0);
+var cartHTML = new Array("<table>
+<tr>
+<th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th> 
+</tr>");
+
+for (var i=0, i < item.length; i++) {
+  cartHTML += "<tr>
+<td><img src='tc_ item .png' alt=' item ' /></td>";}
+ 
+cartHTML += "<td> description </td>
+<td>$ price </td>
+<td> quantity </td>";
+
+cartHTML.orderTotal += "<tr>
+<td colspan='4'>Subtotal</td>
+<td>$ total </td>
+</tr>
+</table>";
+
